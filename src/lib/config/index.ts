19a5d1689308c913351d71f3d587e821d770beb4
Michael Dito <mcdito13@gmail.com>
2020-10-21 02:08:26 -0700

    fix: travis

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,6 +1,6 @@
 import * as Config from './config.json';
 
-const env = process.env.NODE_ENV || 'dev';
+const env = process.env.NODE_ENV || 'test';
 const envConfig = Config[env];
 
 Object.keys(envConfig).forEach((key) => {
