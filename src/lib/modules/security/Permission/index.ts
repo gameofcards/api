9ce6f4b60f4cb15182133321e974f8fcde2a3947
Michael Dito <mcdito13@gmail.com>
2020-10-25 18:29:56 -0700

    fix(tests): import error fix

--- src/lib/modules/security/Permission/index.ts
+++ src/lib/modules/security/Permission/index.ts
@@ -1,7 +1,6 @@
 import Permission from './Permission';
-import { PermissionNames } from './Permission.data';
 import { getModelForClass } from '@typegoose/typegoose';
 
 const PermissionModel = getModelForClass(Permission);
 
-export { Permission, PermissionModel, PermissionNames };
+export { Permission, PermissionModel };
